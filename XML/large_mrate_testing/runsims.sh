27f9cfa75b7cdd7464cfba7f4a3e781d187aa2cd
andre <andre-lichtsteiner@users.noreply.github.com>
2017-02-16 15:27:25 +1300

    Tidied up the folder a little

--- XML/large_mrate_testing/runsims.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for m in 0.125 0.25 0.5 1 2 4 8 16 32 64; do
-    beast -main beast.evolution.tree.coalescent.StructuredCoalescentMultiTypeTree $m
-    mv heights.txt heights$m.txt
-done
